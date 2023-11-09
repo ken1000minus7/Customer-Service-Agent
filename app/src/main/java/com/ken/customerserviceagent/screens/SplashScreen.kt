b654a1470cb45c1ca3fdc5c202195368921ad93b
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2023-11-10 01:39:43 +0530

    Splash screen completed

--- app/src/main/java/com/ken/customerserviceagent/screens/SplashScreen.kt
+++ app/src/main/java/com/ken/customerserviceagent/screens/SplashScreen.kt
@@ -1,14 +1,31 @@
 package com.ken.customerserviceagent.screens
 
 import android.content.Context
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.ken.customerserviceagent.R
 import com.ken.customerserviceagent.data.Constants
 import com.ken.customerserviceagent.data.Routes
 import kotlinx.coroutines.delay
@@ -28,10 +45,48 @@ fun SplashScreen(navController: NavController) {
             }
         }
     }
-    Text(text = Routes.SPLASH_SCREEN)
+
+    val composition = rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.agent)
+    )
+    val progress = animateLottieCompositionAsState(
+        composition = composition.value,
+        iterations = 1
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        LottieAnimation(
+            composition = composition.value,
+            progress = progress.value,
+            modifier = Modifier
+                .size(250.dp)
+                .padding(10.dp),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Customer Service Agent",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Text(
+                text = "Developed by Manjot Singh Oberoi",
+                fontSize = 12.sp,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun SplashScreenPreview() {
     SplashScreen(rememberNavController())
